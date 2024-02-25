b7f0d843cf4b104bf6762acde3a70b139694e5f7
aauroraaborealisrs <imaginepeach@mail.ru>
2024-02-25 20:30:57 +0300

    refactor: translated controller.js to .ts

--- news-JS/src/components/controller/controller.ts
+++ news-JS/src/components/controller/controller.ts
@@ -1,7 +1,8 @@
 import AppLoader from './appLoader';
+import { NewsItem } from '../../types/index';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: (data: NewsItem[]) => void) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,20 +11,20 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: MouseEvent, callback: (data: NewsItem[]) => void) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
-                    newsContainer.setAttribute('data-source', sourceId);
+                    newsContainer.setAttribute('data-source', sourceId!);
                     super.getResp(
                         {
                             endpoint: 'everything',
                             options: {
-                                sources: sourceId,
+                                sources: sourceId!,
                             },
                         },
                         callback
@@ -31,7 +32,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
