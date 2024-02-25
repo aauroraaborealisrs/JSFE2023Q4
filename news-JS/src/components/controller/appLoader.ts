022b87a0f2b15832ab597f7dc56a9874747eb98e
aauroraaborealisrs <imaginepeach@mail.ru>
2024-02-25 20:22:08 +0300

    refactor: translated appLoader.js to .ts

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super(process.env.API_URL, {
+        super(process.env.API_URL!, {
             apiKey: '08354b6d9e134f3b82c8a715f14853e9',
         });
     }
