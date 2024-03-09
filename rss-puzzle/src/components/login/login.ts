2c6f3ef27bebcc70af430676ff70b865214623eb
aauroraaborealisrs <imaginepeach@mail.ru>
2024-03-10 00:02:21 +0300

    refactor: add some files bacuse git tells me its unstaged

--- rss-puzzle/src/components/login/login.ts
+++ rss-puzzle/src/components/login/login.ts
@@ -1,13 +1,13 @@
 import './login.css';
 
 class LoginForm {
-    constructor() {
-        this.render();
-        this.addEventListeners();
-    }
+  constructor() {
+    this.render();
+    this.addEventListeners();
+  }
 
-    render() {
-        const form = `
+  render() {
+    const form = `
             <form id="login-form">
                 <p class="login__header">Welcome!</p>
                 <input class="login__input" type="text" id="firstName" name="firstName" placeholder="First name" required>
@@ -15,36 +15,38 @@ class LoginForm {
                 <button class="button__main submit-button" type="submit">Login</button>
             </form>
         `;
-        document.getElementById('app').innerHTML = form;
-    }
+    document.getElementById('app').innerHTML = form;
+  }
 
-    addEventListeners() {
-        const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('#login-form input');
-        const submitButton: HTMLButtonElement | null = document.querySelector('.submit-button');
+  addEventListeners() {
+    const inputs: NodeListOf<HTMLInputElement> =
+      document.querySelectorAll('#login-form input');
+    const submitButton: HTMLButtonElement | null =
+      document.querySelector('.submit-button');
 
-        const checkInputs = () => {
-            let allFilled = true;
-            inputs.forEach(input => {
-                if (input.value.trim() === '') {
-                    allFilled = false;
-                }
-            });
+    const checkInputs = () => {
+      let allFilled = true;
+      inputs.forEach((input) => {
+        if (input.value.trim() === '') {
+          allFilled = false;
+        }
+      });
 
-            if (allFilled && submitButton) {
-                submitButton.style.backgroundColor = 'black';
-                submitButton.style.color = 'white';
-            } else if (submitButton) {
-                submitButton.style.backgroundColor = 'darkgrey';
-                submitButton.style.color = 'black';
-            }
-        };
+      if (allFilled && submitButton) {
+        submitButton.style.backgroundColor = 'black';
+        submitButton.style.color = 'white';
+      } else if (submitButton) {
+        submitButton.style.backgroundColor = 'darkgrey';
+        submitButton.style.color = 'black';
+      }
+    };
 
-        inputs.forEach(input => {
-            input.addEventListener('input', checkInputs);
-        });
+    inputs.forEach((input) => {
+      input.addEventListener('input', checkInputs);
+    });
 
-        checkInputs();
-    }
+    checkInputs();
+  }
 }
 
 export default LoginForm;
