f7f501dbe6fb5c56e6ee7ab5f697f6dd78bfdc25
aauroraaborealisrs <111872242+aauroraaborealisrs@users.noreply.github.com>
2024-03-09 20:11:34 +0300

    feat: RSS PZ 00 config bundlers (#15)
    * feat: add test configure for webpack and prettier
    
    * Initial Commit
    
    * test:test
    
    * feat: add test husky config
    
    * feat: add config for husky
    
    * feat: fix webpack config
    
    * feat: add formatting
    
    * feat: add .gitignore

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/dist`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
