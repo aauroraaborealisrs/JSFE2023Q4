d3d5affca29b562d4a87acf7a3a3933a6fb9d645
aauroraaborealisrs <imaginepeach@mail.ru>
2024-03-11 00:37:56 +0300

    feat: add personalized greeting

--- rss-puzzle/src/components/start/start.ts
+++ rss-puzzle/src/components/start/start.ts
@@ -1,6 +1,15 @@
 import './start.css';
 import MainPage from '../main/main';
 
+function getUserGreeting() {
+ const userData = localStorage.getItem('userData');
+ if (userData) {
+    const { firstName, surname } = JSON.parse(userData);
+    return `${firstName} ${surname}`;
+ }
+ return null;
+}
+
 class StartScreen {
   constructor() {
     this.render();
@@ -8,6 +17,13 @@ class StartScreen {
   }
 
   render() {
+
+    const userGreeting = getUserGreeting();
+    let greetingMessage = '';
+    if (userGreeting) {
+      greetingMessage = `<p class="greeting">Welcome back, ${userGreeting}!</p>`;
+    }
+
     const startScreen = `
       <div id="start-screen">
         <header class="start__header">
@@ -16,6 +32,7 @@ class StartScreen {
             Unlock English grammar mastery one puzzle piece at a time with our innovative learning app!
           </p>
         </header>
+        ${greetingMessage}
         <button class="start-button">Start Learning</button>
       </div>
     `;
