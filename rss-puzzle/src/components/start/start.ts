79b80f4a932738e14c389557782964639868859e
aauroraaborealisrs <imaginepeach@mail.ru>
2024-03-10 23:58:24 +0300

    feat: add start button

--- rss-puzzle/src/components/start/start.ts
+++ rss-puzzle/src/components/start/start.ts
@@ -1,22 +1,34 @@
 import './start.css';
+import MainPage from '../main/main';
 
 class StartScreen {
   constructor() {
     this.render();
+    this.addEventListeners(); 
   }
 
   render() {
     const startScreen = `
-        <div id="start-screen">
+      <div id="start-screen">
         <header class="start__header">
-        <h1 class="app-name">RSS Puzzle</h1>
-        <p class="game-description">
-        Unlock English grammar mastery one puzzle piece at a time with our innovative learning app!
-        </p>
+          <h1 class="app-name">RSS Puzzle</h1>
+          <p class="game-description">
+            Unlock English grammar mastery one puzzle piece at a time with our innovative learning app!
+          </p>
         </header>
-    </div>
+        <button class="start-button">Start Learning</button>
+      </div>
     `;
     document.getElementById('app').innerHTML = startScreen;
+  }
+
+  addEventListeners() {
+    const startButton = document.querySelector('.start-button');
+    if (startButton) {
+      startButton.addEventListener('click', () => {
+        const mainPage = new MainPage();
+      });
+    }
   }
 }
 
